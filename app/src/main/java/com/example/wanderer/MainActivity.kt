186c685b837d52c681d72f6a65bfb8620264f1de
Introhart <introhart@gmail.com>
2021-03-05 03:54:17 +0700

    Bottom Navigation Menu added

--- app/src/main/java/com/example/wanderer/MainActivity.kt
+++ app/src/main/java/com/example/wanderer/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.wanderer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationMenu : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationMenu = findViewById(R.id.bottom_nav);
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
+
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.page_explore -> {
+                Toast.makeText(applicationContext, "Explore", Toast.LENGTH_SHORT).show()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.page_map -> {
+                Toast.makeText(applicationContext, "Map", Toast.LENGTH_SHORT).show()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.page_profile -> {
+                Toast.makeText(applicationContext, "Profile", Toast.LENGTH_SHORT).show()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
